3d689216e4af65aefe17db0af575fb67a031bb52
Sung Ho Kim <likeleon@gmail.com>
2018-11-03 12:14:33 +0900

    템플릿 제거 중간 서밋

--- Geometry/Public/Box.h
+++ Geometry/Public/Box.h
@@ -1,82 +1,38 @@
 ﻿#pragma once
 
-#include "Geometry/Public/Access.h"
-#include "Geometry/Public/PointType.h"
-#include "Geometry/Public/Tag.h"
-#include "Geometry/Public/Tags.h"
+#include "Math/Vec3.h"
 
 namespace Geometry {
 namespace Index {
 
-template <typename Point>
 class Box {
 public:
 	Box () = default;
 
-	Box (const Point& min_corner, const Point& max_corner)
+	Box (const Math::Vec3& min_corner, const Math::Vec3& max_corner)
 		: min_corner_(min_corner), max_corner_(max_corner) {
 	}
 
-	const Point& min_corner () const {
+	const Math::Vec3& min_corner () const {
 		return min_corner_;
 	}
 	
-	Point& min_corner () {
+	Math::Vec3& min_corner () {
 		return min_corner_;
 	}
 
-	const Point& max_corner () const {
+	const Math::Vec3& max_corner () const {
 		return max_corner_;
 	}
 
-	Point& max_corner () {
+	Math::Vec3& max_corner () {
 		return max_corner_;
 	}
 
 private:
-	Point min_corner_;
-	Point max_corner_;
+	Math::Vec3 min_corner_;
+	Math::Vec3 max_corner_;
 };
-
-namespace Traits {
-
-template <typename Point>
-struct Tag<Box<Point>> {
-	using Type = BoxTag;
-};
-
-template <typename Point>
-struct PointType<Box<Point>> {
-	using Type = Point;
-};
-
-template <typename Point, std::size_t Dimension>
-struct IndexedAccess<Box<Point>, 0, Dimension> {
-	using CoordinateType = typename CoordinateType<Point>::Type;
-
-	static CoordinateType Get (const Box<Point>& box) {
-		return Index::Get<Dimension>(box.min_corner());
-	}
-
-	static void Set (Box<Point>& box, const CoordinateType& value) {
-		Index::Set<Dimension>(box.min_corner(), value);
-	}
-};
-
-template <typename Point, std::size_t Dimension>
-struct IndexedAccess<Box<Point>, 1, Dimension> {
-	using CoordinateType = typename CoordinateType<Point>::Type;
-
-	static CoordinateType Get (const Box<Point>& box) {
-		return Index::Get<Dimension>(box.max_corner());
-	}
-
-	static void Set (Box<Point>& box, const CoordinateType& value) {
-		Index::Set<Dimension>(box.max_corner(), value);
-	}
-};
-
-} // namespace Traits
 
 } // namespace Index
 } // namespace Geometry
---
+++ Math/Vec3.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+namespace Math {
+
+struct Vec3 {
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+};
+
+} // namespace Math
